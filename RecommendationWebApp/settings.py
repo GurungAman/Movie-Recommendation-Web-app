64c288ebad4c2c21502b8308826ce4bee285f775
Aman Gurung <amenag30@gmail.com>
2021-04-02 11:28:11 +0545

    changes for database configuration

--- RecommendationWebApp/settings.py
+++ RecommendationWebApp/settings.py
@@ -76,23 +76,27 @@ WSGI_APPLICATION = 'RecommendationWebApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'recommenderwebapp',
-        'USER': 'aman',
-        'PASSWORD': 'amangrg123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+ 
+import dj_databse_url
+db_from_env = dj_databse_url.config()
+DATABASES['default'].update()
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'recommenderwebapp',
+#         'USER': 'aman',
+#         'PASSWORD': 'amangrg123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
